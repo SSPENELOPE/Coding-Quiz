f6f5aa49eedb41a663de5bf65f24c031a2fbde48
SSPENELOPE <typoepping@gmail.com>
2022-07-21 13:50:22 -0400

    added javascript questions and html buttons

---
+++ assets/script.js
@@ -0,0 +1,122 @@
+// Create an array of objects that hold questions and answers
+var questions = [
+    {
+        id: 0,
+        q: "What is the proper HTML tag to use for a image element?"
+        // Create and array for the answers
+        a: [
+            { text: "<picture>", isCorrect: false },
+            { text: "<img>", isCorrect: true },
+            { text: "<image>", isCorrect: false },
+            { text: "<photo>", isCorrect: false }
+           ]
+    };
+
+    {
+        id: 1,
+        q: "What is the proper way to annotate a class in css?"
+        // Create an array for the answers
+        a: [
+            { text: ".class", isCorrect: true },
+            { text: "#class", isCorrect: false },
+            { text: "*class", isCorrect: false },
+            { text: "$class", isCorrect: false } 
+        ]
+    };
+
+    {
+        id: 2,
+        q: "What is the proper method to insert an item into the beginning of an array?"
+        // Create an array for the answers
+        a: [
+            { text: ".splice()", isCorrect: false },
+            { text: ".pop()", isCorrect: false },
+            { text: ".concact()", isCorrect: false },
+            { text: ".unshift()", isCorrect: true }
+        ]
+    };
+//TODO:
+    {
+        id: 3,
+        q: "What is the proper method to insert an item into the beginning of an array?"
+        // Create an array for the answers
+        a: [
+            { text: ".splice()", isCorrect: false },
+            { text: ".pop()", isCorrect: false },
+            { text: ".concact()", isCorrect: false },
+            { text: ".unshift()", isCorrect: true }
+        ]
+    };
+
+    {
+        id: 4,
+        q: "What is the proper method to insert an item into the beginning of an array?"
+        // Create an array for the answers
+        a: [
+            { text: ".splice()", isCorrect: false },
+            { text: ".pop()", isCorrect: false },
+            { text: ".concact()", isCorrect: false },
+            { text: ".unshift()", isCorrect: true }
+        ]
+    };
+
+    {
+        id: 5,
+        q: "What is the proper method to insert an item into the beginning of an array?"
+        // Create an array for the answers
+        a: [
+            { text: ".splice()", isCorrect: false },
+            { text: ".pop()", isCorrect: false },
+            { text: ".concact()", isCorrect: false },
+            { text: ".unshift()", isCorrect: true }
+        ]
+    };
+
+    {
+        id: 6,
+        q: "What is the proper method to insert an item into the beginning of an array?"
+        // Create an array for the answers
+        a: [
+            { text: ".splice()", isCorrect: false },
+            { text: ".pop()", isCorrect: false },
+            { text: ".concact()", isCorrect: false },
+            { text: ".unshift()", isCorrect: true }
+        ]
+    };
+
+    {
+        id: 7,
+        q: "What is the proper method to insert an item into the beginning of an array?"
+        // Create an array for the answers
+        a: [
+            { text: ".splice()", isCorrect: false },
+            { text: ".pop()", isCorrect: false },
+            { text: ".concact()", isCorrect: false },
+            { text: ".unshift()", isCorrect: true }
+        ]
+    };
+
+    {
+        id: 8,
+        q: "What is the proper method to insert an item into the beginning of an array?"
+        // Create an array for the answers
+        a: [
+            { text: ".splice()", isCorrect: false },
+            { text: ".pop()", isCorrect: false },
+            { text: ".concact()", isCorrect: false },
+            { text: ".unshift()", isCorrect: true }
+        ]
+    };
+
+    {
+        id: 9,
+        q: "What is the proper method to insert an item into the beginning of an array?"
+        // Create an array for the answers
+        a: [
+            { text: ".splice()", isCorrect: false },
+            { text: ".pop()", isCorrect: false },
+            { text: ".concact()", isCorrect: false },
+            { text: ".unshift()", isCorrect: true }
+        ]
+    };
+];
