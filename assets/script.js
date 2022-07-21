77e445f70fb3f65e196b3c666cf172a5ba75b72e
SSPENELOPE <typoepping@gmail.com>
2022-07-21 18:43:44 -0400

    updated JS

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 var questions = [
     {
         id: 0,
-        q: "What is the proper HTML tag to use for a image element?"
+        q: "What is the proper HTML tag to use for a image element?",
         // Create and array for the answers
         a: [
             { text: "<picture>", isCorrect: false },
@@ -10,11 +10,11 @@ var questions = [
             { text: "<image>", isCorrect: false },
             { text: "<photo>", isCorrect: false }
            ]
-    };
+    },
 
     {
         id: 1,
-        q: "What is the proper way to annotate a class in css?"
+        q: "What is the proper way to annotate a class in css?",
         // Create an array for the answers
         a: [
             { text: ".class", isCorrect: true },
@@ -22,11 +22,11 @@ var questions = [
             { text: "*class", isCorrect: false },
             { text: "$class", isCorrect: false } 
         ]
-    };
+    },
 
     {
         id: 2,
-        q: "What is the proper method to insert an item into the beginning of an array?"
+        q: "What is the proper method to insert an item into the beginning of an array?",
         // Create an array for the answers
         a: [
             { text: ".splice()", isCorrect: false },
@@ -34,11 +34,11 @@ var questions = [
             { text: ".concact()", isCorrect: false },
             { text: ".unshift()", isCorrect: true }
         ]
-    };
+    },
 //TODO:
     {
         id: 3,
-        q: "What is the proper method to insert an item into the beginning of an array?"
+        q: "What is the proper method to insert an item into the beginning of an array?",
         // Create an array for the answers
         a: [
             { text: ".splice()", isCorrect: false },
@@ -46,11 +46,11 @@ var questions = [
             { text: ".concact()", isCorrect: false },
             { text: ".unshift()", isCorrect: true }
         ]
-    };
+    },
 
     {
         id: 4,
-        q: "What is the proper method to insert an item into the beginning of an array?"
+        q: "What is the proper method to insert an item into the beginning of an array?",
         // Create an array for the answers
         a: [
             { text: ".splice()", isCorrect: false },
@@ -58,11 +58,11 @@ var questions = [
             { text: ".concact()", isCorrect: false },
             { text: ".unshift()", isCorrect: true }
         ]
-    };
+    },
 
     {
         id: 5,
-        q: "What is the proper method to insert an item into the beginning of an array?"
+        q: "What is the proper method to insert an item into the beginning of an array?",
         // Create an array for the answers
         a: [
             { text: ".splice()", isCorrect: false },
@@ -70,11 +70,11 @@ var questions = [
             { text: ".concact()", isCorrect: false },
             { text: ".unshift()", isCorrect: true }
         ]
-    };
+    },
 
     {
         id: 6,
-        q: "What is the proper method to insert an item into the beginning of an array?"
+        q: "What is the proper method to insert an item into the beginning of an array?",
         // Create an array for the answers
         a: [
             { text: ".splice()", isCorrect: false },
@@ -82,11 +82,11 @@ var questions = [
             { text: ".concact()", isCorrect: false },
             { text: ".unshift()", isCorrect: true }
         ]
-    };
+    },
 
     {
         id: 7,
-        q: "What is the proper method to insert an item into the beginning of an array?"
+        q: "What is the proper method to insert an item into the beginning of an array?",
         // Create an array for the answers
         a: [
             { text: ".splice()", isCorrect: false },
@@ -94,11 +94,11 @@ var questions = [
             { text: ".concact()", isCorrect: false },
             { text: ".unshift()", isCorrect: true }
         ]
-    };
+    },
 
     {
         id: 8,
-        q: "What is the proper method to insert an item into the beginning of an array?"
+        q: "What is the proper method to insert an item into the beginning of an array?",
         // Create an array for the answers
         a: [
             { text: ".splice()", isCorrect: false },
@@ -106,11 +106,11 @@ var questions = [
             { text: ".concact()", isCorrect: false },
             { text: ".unshift()", isCorrect: true }
         ]
-    };
+    },
 
     {
         id: 9,
-        q: "What is the proper method to insert an item into the beginning of an array?"
+        q: "What is the proper method to insert an item into the beginning of an array?",
         // Create an array for the answers
         a: [
             { text: ".splice()", isCorrect: false },
@@ -118,5 +118,9 @@ var questions = [
             { text: ".concact()", isCorrect: false },
             { text: ".unshift()", isCorrect: true }
         ]
-    };
+    }
 ];
+
+function start() {
+    var start = document.getElementById("quiz")
+}
