745622cf2a0ff2500bb647c16baae710caccb48c
SSPENELOPE <typoepping@gmail.com>
2022-07-26 16:58:40 -0400

    most recent code

--- assets/script.js
+++ assets/script.js
@@ -1,103 +1,103 @@
 // Questions for the quiz using an Array of Objects
 var questions = [
-{
-    question: "What do we use to change the style of an elment in JavaScript?",
-    answer: [{ text: ".setAttribut()", isCorrect: true },
-    { text: ".splice()", isCorrect: false },
-    { text: ".querySelector", isCorrect: false },
-    { text: ".elementStyle", isCorrect: false }
-    ]
+    {
+        question: "What do we use to change the style of an elment in JavaScript?",
+        answer: [{ text: ".setAttribut()", isCorrect: true },
+        { text: ".splice()", isCorrect: false },
+        { text: ".querySelector", isCorrect: false },
+        { text: ".elementStyle", isCorrect: false }
+        ]
 
-},
-{
-    question: "How do you declare a string in JavaScript?",
-    answer: [{ text: "var myVar = []", isCorrect: false, },
-    { text: "var myVar = {}", isCorrect: false },
-    { text: "var myVar = \"\"", isCorrect: true },
-    { text: "var myVar = ()", isCorrect: false }
-    ]
+    },
+    {
+        question: "How do you declare a string in JavaScript?",
+        answer: [{ text: "var myVar = []", isCorrect: false, },
+        { text: "var myVar = {}", isCorrect: false },
+        { text: "var myVar = \"\"", isCorrect: true },
+        { text: "var myVar = ()", isCorrect: false }
+        ]
 
-},
-{
-    question: "How do we tell a number to \"increment\" in JavaScript?",
-    answer: [{ text: "i--", isCorrect: false },
-    { text: "i-=", isCorrect: false },
-    { text: "i+=", isCorrect: false },
-    { text: "i++", isCorrect: true }
-    ]
+    },
+    {
+        question: "How do we tell a number to \"increment\" in JavaScript?",
+        answer: [{ text: "i--", isCorrect: false },
+        { text: "i-=", isCorrect: false },
+        { text: "i+=", isCorrect: false },
+        { text: "i++", isCorrect: true }
+        ]
 
-},
-{
-    question: "How do you delare function in JavaScript?",
-    answer: [{ text: "function\"\"", isCorrect: false },
-    { text: "function()", isCorrect: true },
-    { text: "function{}", isCorrect: false },
-    { text: "function[]", isCorrect: false }
-    ]
+    },
+    {
+        question: "How do you delare function in JavaScript?",
+        answer: [{ text: "function\"\"", isCorrect: false },
+        { text: "function()", isCorrect: true },
+        { text: "function{}", isCorrect: false },
+        { text: "function[]", isCorrect: false }
+        ]
 
-},
+    },
 
-{
-    question: "What is the method for removing the last item from an array?",
-    answer: [{ text: ".unshift()", isCorrect: false },
-    { text: ".slice()", isCorrect: false },
-    { text: ".pop()", isCorrect: true },
-    { text: ".push()", isCorrect: false }
-    ]
+    {
+        question: "What is the method for removing the last item from an array?",
+        answer: [{ text: ".unshift()", isCorrect: false },
+        { text: ".slice()", isCorrect: false },
+        { text: ".pop()", isCorrect: true },
+        { text: ".push()", isCorrect: false }
+        ]
 
-},
+    },
 
-{
-    question: "What is the correct operator symbol to use a \"or\" comparison?",
-    answer: [{ text: "&&", isCorrect: false },
-    { text: "||", isCorrect: true },
-    { text: "++", isCorrect: false },
-    { text: "--", isCorrect: false }
-    ]
+    {
+        question: "What is the correct operator symbol to use a \"or\" comparison?",
+        answer: [{ text: "&&", isCorrect: false },
+        { text: "||", isCorrect: true },
+        { text: "++", isCorrect: false },
+        { text: "--", isCorrect: false }
+        ]
 
-},
+    },
 
-{
-    question: "What is the comparison operator used to tell if something is equal in \"type\" and \"value\"",
-    answer: [{ text: "=", isCorrect: false },
-    { text: "==", isCorrect: false },
-    { text: "===", isCorrect: true },
-    { text: "====", isCorrect: false }
-    ]
+    {
+        question: "What is the comparison operator used to tell if something is equal in \"type\" and \"value\"",
+        answer: [{ text: "=", isCorrect: false },
+        { text: "==", isCorrect: false },
+        { text: "===", isCorrect: true },
+        { text: "====", isCorrect: false }
+        ]
 
-},
+    },
 
-{
-    question: "Which of these is the correct way to create an element in JavaScript?",
-    answer: [{ text: "document.createTag", isCorrect: false },
-    { text: "document.createDiv", isCorrect: false },
-    { text: "document.elementCreate", isCorrect: false },
-    { text: "document.createElement", isCorrect: true }
-    ]
+    {
+        question: "Which of these is the correct way to create an element in JavaScript?",
+        answer: [{ text: "document.createTag", isCorrect: false },
+        { text: "document.createDiv", isCorrect: false },
+        { text: "document.elementCreate", isCorrect: false },
+        { text: "document.createElement", isCorrect: true }
+        ]
 
-},
+    },
 
-{
-    question: "Which of these is the correct way to declare a boolean in JavaScript?",
-    answer: [{ text: "var myVar = true;", isCorrect: true },
-    { text: "var myVar = 9;", isCorrect: false },
-    { text: "var myVar = \"nine\"", isCorrect: false },
-    { text: "var myVar = [\"1\", \"2\"]", isCorrect: false }
-    ]
+    {
+        question: "Which of these is the correct way to declare a boolean in JavaScript?",
+        answer: [{ text: "var myVar = true;", isCorrect: true },
+        { text: "var myVar = 9;", isCorrect: false },
+        { text: "var myVar = \"nine\"", isCorrect: false },
+        { text: "var myVar = [\"1\", \"2\"]", isCorrect: false }
+        ]
 
-},
+    },
 
-{
-    question: "What is the unit of meaurment when setting time intervals for a timer in JavaScript?",
-    answers: [{ text: "seconds", isCorrect: false },
-    { text: "milliseconds", isCorrect: true },
-    { text: "minutes", isCorrect: false },
-    { text: "hours", isCorrect: false }
-    ]
-},
-
-
+    {
+        question: "What is the unit of meaurment when setting time intervals for a timer in JavaScript?",
+        answers: [{ text: "seconds", isCorrect: false },
+        { text: "milliseconds", isCorrect: true },
+        { text: "minutes", isCorrect: false },
+        { text: "hours", isCorrect: false }
+        ]
+    },
 ];
+
+
 
 
 // Variables
@@ -145,17 +145,14 @@ function countdown() {
 };
 
 
-// Random question grab function
-function getRandomNumber() {
-    for (var i = 0; i < questions.length; i++){
-    return randomIndex = questions[Math.floor(Math.random() * questions.length)];
-    }
+// Function to obtain a object randomly from questions array
+function getRandomQuestion(questions) {
+       return randomIndex = questions[Math.floor(Math.random() * questions.length)];
 };
+   
 
-
-// Iterate Function to cylce the questions at a random index from last function
+// Iterate Function to cylce the questions at a randomIndex from getRandomNumber function
 function iterate(randomIndex) {
-
     console.log(questions);
     questions.pop(randomIndex);
 
@@ -163,6 +160,12 @@ function iterate(randomIndex) {
     question.innerText = questions[0].question;
     question.innerText = questions[1].question;
     question.innerText = questions[2].question;
+    question.innerText = questions[3].question;
+    question.innerText = questions[4].question;
+    question.innerText = questions[5].question;
+    question.innerText = questions[6].question;
+    question.innerText = questions[7].question;
+    question.innerText = questions[8].question;
 
     // Add the text we want to display from our Questions array
     answer1.innerText = questions[0].answer[0].text;
@@ -179,6 +182,36 @@ function iterate(randomIndex) {
     answer2.innerText = questions[2].answer[1].text;
     answer3.innerText = questions[2].answer[2].text;
     answer4.innerText = questions[2].answer[3].text;
+
+    answer1.innerText = questions[3].answer[0].text;
+    answer2.innerText = questions[3].answer[1].text;
+    answer3.innerText = questions[3].answer[2].text;
+    answer4.innerText = questions[2].answer[3].text;
+
+    answer1.innerText = questions[4].answer[0].text;
+    answer2.innerText = questions[4].answer[1].text;
+    answer3.innerText = questions[4].answer[2].text;
+    answer4.innerText = questions[4].answer[3].text;
+
+    answer1.innerText = questions[5].answer[0].text;
+    answer2.innerText = questions[5].answer[1].text;
+    answer3.innerText = questions[5].answer[2].text;
+    answer4.innerText = questions[5].answer[3].text;
+
+    answer1.innerText = questions[6].answer[0].text;
+    answer2.innerText = questions[6].answer[1].text;
+    answer3.innerText = questions[6].answer[2].text;
+    answer4.innerText = questions[6].answer[3].text;
+
+    answer1.innerText = questions[7].answer[0].text;
+    answer2.innerText = questions[7].answer[1].text;
+    answer3.innerText = questions[7].answer[2].text;
+    answer4.innerText = questions[7].answer[3].text;
+
+    answer1.innerText = questions[8].answer[0].text;
+    answer2.innerText = questions[8].answer[1].text;
+    answer3.innerText = questions[8].answer[2].text;
+    answer4.innerText = questions[8].answer[3].text;
 
 
     // Add true or false using isCorrect boolean
@@ -197,6 +230,36 @@ function iterate(randomIndex) {
     answer3.value = questions[2].answer[2].isCorrect;
     answer4.value = questions[2].answer[3].isCorrect;
 
+    answer1.value = questions[3].answer[0].isCorrect;
+    answer2.value = questions[3].answer[1].isCorrect;
+    answer3.value = questions[3].answer[2].isCorrect;
+    answer4.value = questions[3].answer[3].isCorrect;
+
+    answer1.value = questions[4].answer[0].isCorrect;
+    answer2.value = questions[4].answer[1].isCorrect;
+    answer3.value = questions[4].answer[2].isCorrect;
+    answer4.value = questions[4].answer[3].isCorrect;
+
+    answer1.value = questions[5].answer[0].isCorrect;
+    answer2.value = questions[5].answer[1].isCorrect;
+    answer3.value = questions[5].answer[2].isCorrect;
+    answer4.value = questions[5].answer[3].isCorrect;
+
+    answer1.value = questions[6].answer[0].isCorrect;
+    answer2.value = questions[6].answer[1].isCorrect;
+    answer3.value = questions[6].answer[2].isCorrect;
+    answer4.value = questions[6].answer[3].isCorrect;
+
+    answer1.value = questions[7].answer[0].isCorrect;
+    answer2.value = questions[7].answer[1].isCorrect;
+    answer3.value = questions[7].answer[2].isCorrect;
+    answer4.value = questions[7].answer[3].isCorrect;
+
+    answer1.value = questions[8].answer[0].isCorrect;
+    answer2.value = questions[8].answer[1].isCorrect;
+    answer3.value = questions[8].answer[2].isCorrect;
+    answer4.value = questions[8].answer[3].isCorrect;
+
 };
 
 
@@ -204,7 +267,7 @@ function iterate(randomIndex) {
 
 // Start button to start the quiz with iterate and countdown function within
 startBtn.addEventListener("click", function () {
-    if (quiz.style.display === "none" && id <= 9) {
+    if (quiz.style.display === "none") {
         quiz.style.display = "flex";
 
     } else {
@@ -212,7 +275,7 @@ startBtn.addEventListener("click", function () {
     }
     start.setAttribute("style", "display: none;");
     countdown();
-    iterate(getRandomNumber());
+    iterate();
 });
 
 
@@ -220,24 +283,24 @@ startBtn.addEventListener("click", function () {
 answers.forEach(answer => {
     answer.addEventListener("click", function () {
         if (answer.value == "true" && questions.length > 0) {
-            iterate(getRandomNumber());
+            iterate(getRandomQuestion());
             result.innerText = "True!";
             result.style.color = "green";
         } else if (answer.value == "false" && questions.length > 0) {
-            iterate(getRandomNumber());
+            iterate(getRandomQuestion());
             timeLeft -= 10;
             result.innerText = "False!";
             result.style.color = "red";
-        }       
+        }
     });
 });
-            
+
 
 // Try again button end of quiz
-tryAgain.addEventListener("click", function() {
+tryAgain.addEventListener("click", function () {
     timeLeft = 120;
     countdown();
-    iterate(getRandomNumber());
+    iterate(randomIndex);
     quiz.style.display = "flex";
     score.style.display = "none";
 });
